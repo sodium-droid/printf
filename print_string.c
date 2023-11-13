788c08a86a8f85c960b998761195e59938a5c479
Samuel Osayomi <samuelosayomi@gmail.com>
2023-11-13 10:37:31 -0800

    Task 0

---
+++ print_string.c
@@ -0,0 +1,51 @@
+#include "main.h"
+/**
+ * _printf - A function that prints
+ * string or character
+ * @format: A pointer that contains the required string or character
+ * Return: Returns number of character printer
+ */
+
+
+int _printf(const char *format, ...)
+{
+	va_list args;
+	int count = 0;
+
+	va_start(args, format);
+
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			switch (*format)
+			{
+				case 'c':
+					count += putchar(va_arg(args, int));
+					break;
+				case 's':
+					{
+					const char *str = va_arg(args, const char *);
+
+					while (*str)
+					{
+						count += putchar(*str);
+						str++;
+					}
+					break;
+					}
+				case '%':
+					count += putchar('%');
+					break;
+				default:
+					break;
+			}
+		}
+		else
+			count += putchar(*format);
+		format++;
+	}
+	va_end(args);
+	return (count);
+}
